cccea8d78e4b91d41faeaa01e01d71e58a86b2c7
Stephan Nef <65945515+nefste@users.noreply.github.com>
2024-05-01 16:06:15 +0200

    Update 02_Kalkulator.py

--- pages/02_Kalkulator.py
+++ pages/02_Kalkulator.py
@@ -21,7 +21,7 @@ import time
 from streamlit_extras.let_it_rain import rain 
 
 import webbrowser
-
+from urllib.parse import quote
 
 
 st.set_page_config(
@@ -245,57 +245,56 @@ if starten:
         
     
     with col22:
-        st.metric("Angebotspreis", value="22’950 CHF", delta="-15%", delta_color="inverse")
+        st.metric("Angebotspreis", value="22’950 CHF", delta="-4'050 CHF", delta_color="inverse")
     
-    st.success("Disscount von 15% im Angebot vorhanden, Ersparnis von 4'050 CHF")
+    st.success("Ersparnis von 4'050 CHF")
         
     
-    # Email details
-    subject = "ListAnalytics Angebot - Swisscom"
-    to = "ann-kathrin.koepple@student.unisg.ch; stephan.nef@student.unisg.ch"
-    body = """Hallo Karin Kunde - Swisscom
+# Email details
+subject = "ListAnalytics Angebot - Swisscom"
+to = "ann-kathrin.koepple@student.unisg.ch; stephan.nef@student.unisg.ch"
+body = """Hallo Karin Kunde - Swisscom
+
+Danke für Ihr interesse an Lista Office Möbel. 
+Es freut uns das wir Ihnen ein attraktives Angebot dank dem ListAnalytics Kalkulator rechnen konnten. 
+
+**********************************************************
+Gerne sende ich Ihnen folgendes Angebot:
+
+---------------------------------------------------------- 
+- Rücknahmen von:
+    - 50 Stück LO Pure Tischen 
     
-    Danke für Ihr interesse an Lista Office Möbel. 
-    Es freut uns das wir Ihnen ein attraktives Angebot dank dem ListAnalytics Kalkulator rechnen konnten. 
+- Kauf von: 
+    - 60 LO Extend Tischen
+    Preis Listenpreis: 450.00 CHF/Stück
+    Reduktion aufgrund Rücknahme: 4'050 CHF
+----------------------------------------------------------  
     
-    **********************************************************
-    Gerne sende ich Ihnen folgendes Angebot:
-    
-    ---------------------------------------------------------- 
-    - Rücknahmen von:
-        - 50 Stück LO Pure Tischen 
-        
-    - Kauf von: 
-        - 60 LO Extend Tischen
-        Preis Listenpreis: 450.00 CHF/Stück
-        Reduktion aufgrund Rücknahme: 15% Discount
-    ----------------------------------------------------------  
-        
-    Total Kosten: 22’950 CHF (ohne Mehrwertsteuer, inkl. Transport und 2 Jahre Garantie)
-    
-    ----------------------------------------------------------
-    **********************************************************
-    
-    
-    Bitte lass uns wissen, ob weitere Informationen benötigen oder wenn wir den Auftrag für Sie vorbereiten sollen.
-    
-    Freundliche Grüsse,
-    Sammy Sales, Lista Office AG
-    
-    
-    ----------------------------------------------------------
-    Disclaimer:
-    Dieses Angebot ist vertraulich und ausschließlich für den Adressaten bestimmt. Wenn Sie nicht der vorgesehene Adressat sind, informieren Sie bitte den Absender sofort und löschen Sie diese E-Mail. Eine Weitergabe, Vervielfältigung oder Nutzung des Inhalts dieser E-Mail ist ohne schriftliche Genehmigung des Absenders nicht gestattet. Dieses Angebot ist unverbindlich und freibleibend. Preise und Konditionen können sich ändern. Für Tippfehler oder Irrtümer in der Kommunikation übernimmt der Absender keine Haftung.
-    ----------------------------------------------------------
-    
-    
-    
-    
-    """
-    
-    # Encode the email components for URL use
-    from urllib.parse import quote
-    
+Total Kosten: 22’950 CHF (ohne Mehrwertsteuer, inkl. Transport und 2 Jahre Garantie)
+
+----------------------------------------------------------
+**********************************************************
+
+
+Bitte lass uns wissen, ob weitere Informationen benötigen oder wenn wir den Auftrag für Sie vorbereiten sollen.
+
+Freundliche Grüsse,
+Sammy Sales, Lista Office AG
+
+
+----------------------------------------------------------
+Disclaimer:
+Dieses Angebot ist vertraulich und ausschließlich für den Adressaten bestimmt. Wenn Sie nicht der vorgesehene Adressat sind, informieren Sie bitte den Absender sofort und löschen Sie diese E-Mail. Eine Weitergabe, Vervielfältigung oder Nutzung des Inhalts dieser E-Mail ist ohne schriftliche Genehmigung des Absenders nicht gestattet. Dieses Angebot ist unverbindlich und freibleibend. Preise und Konditionen können sich ändern. Für Tippfehler oder Irrtümer in der Kommunikation übernimmt der Absender keine Haftung.
+----------------------------------------------------------
+
+
+
+
+"""
+ 
+with st.expander("Angebot erstellen"):
+
     def create_mailto_link(to, subject, body):
         subject_encoded = quote(subject)
         body_encoded = quote(body)
@@ -303,6 +302,7 @@ if starten:
     
     # Streamlit button to open the email in Outlook
     if st.button('Sende Angebot'):
+        starten = True
         link = create_mailto_link(to, subject, body)
         webbrowser.open(link)
         st.info('Ein Outlook-Fenster sollte sich mit Ihrer E-Mail-Vorlage öffnen.')
